b80584de7cd3aaf2f10032bf812dfe2ce1cd8c4b
q0sul3sul3 <q0sul3sul3@gmail.com>
2022-01-18 19:05:26 +0800

    fix error

--- web_crawler.py
+++ web_crawler.py
@@ -15,20 +15,20 @@ args = parser.parse_args()
 
 # keyword = '資料科學' # 搜尋含有'資料科學'的文章
 src = "https://www.ptt.cc/bbs/{}/search?q=".format(args.table) + args.keyword
-
 r = requests.get(src) # get 此頁面的 HTML
 soup = bs4.BeautifulSoup(r.text, "html.parser") # 用bs4 解析html 資料格式
 print(dt.date.today())
+
 if args.write:
 
     file = open('/tmp/{}.csv'.format(args.keyword), 'w')
 
-    # for j in soup.find_all('div', class_="r-ent"):
-    #     if j.span and int(j.span.string) >= 10: # 有推文數 and 推文數 > 10
-    # #     if j.span and j.span['class']==['hl', 'f3']:
-    #         print('ptt.cc' + j.a['href'], j.span.string, j.a.string)
-    #         data = 'ptt.cc' + j.a['href'] + ' ' + j.span.string + ' ' + j.a.string + '\n'
-    #         file.write(data)
+    for j in soup.find_all('div', class_="r-ent"):
+#         if j.span and int(j.span.string) >= 10: # 有推文數 and 推文數 > 10
+        if j.span and (j.span['class']==['hl', 'f3'] or j.span['class']==['hl', 'f1']):
+            print('ptt.cc' + j.a['href'], j.span.string, j.a.string)
+            data = 'ptt.cc' + j.a['href'] + ' ' + j.span.string + ' ' + j.a.string + '\n'
+            file.write(data)
     prepage = soup.find('a', class_="btn wide", string = "‹ 上頁") # 如果沒有上頁 --> None
 
     while prepage:
